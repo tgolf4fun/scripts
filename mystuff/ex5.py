f3d8830e277c1ba1019b7a225a94d40c11b01f59
Tracy Thorn <tracy.a.thorn@disney.com>
2015-09-02 20:59:37 -0700

    exercise 5, still fun

---
+++ mystuff/ex5.py
@@ -0,0 +1,17 @@
+my_name='Jeffie Lou'
+my_age=28 #not really
+my_height=69 #yep
+my_weight=172 #for real
+my_eyes= 'brown' #yessir
+my_teeth='Always white'
+my_hair='beautiful'
+
+print "Lets talk about %s." % my_name
+print "Cutie is %d inches tall." % my_height
+print "HM Queen is %d pounds." % my_weight
+print "Actually that is perfect to her husband"
+print "%s got %s eyes." % (my_name,  my_eyes)
+print "%s\'s teeth are usually %s but .........." % (my_name, my_teeth)
+
+#This is the tricky line
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
